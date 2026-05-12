c78854d3149d1e4e0a04b649e078858eb41306f5
Lalit Vohra <lalitvohra2000@gmail.com>
2026-05-13 03:13:21 +0530

    TallyVox v21 release assets

---
+++ app/build/generated/source/buildConfig/release/com/tallyvox/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.tallyvox;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.tallyvox";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 21;
+  public static final String VERSION_NAME = "1.0.21";
+}
